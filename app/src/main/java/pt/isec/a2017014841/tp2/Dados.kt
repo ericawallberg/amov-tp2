ce3fbab089eadf99b2ddfafed09f707e6d4dd72b
onikenx <onikenx@protonmail.com>
2021-01-13 19:34:21 +0000

    comunicacoes acho que estao a funcar prximo: coords e firebase db

--- app/src/main/java/pt/isec/a2017014841/tp2/Dados.kt
+++ app/src/main/java/pt/isec/a2017014841/tp2/Dados.kt
@@ -2,7 +2,7 @@ package pt.isec.a2017014841.tp2
 
 public object Dados {
     const val MIN_USERS = 2;
-    const val SERVER_MODE = 1;
+    const val SERVER_MODE = 0;
     const val CLIENT_MODE = 1;
 
 }
---
+++ app/src/main/java/pt/isec/a2017014841/tp2/helpers/ServerConnectionManager.kt
@@ -0,0 +1,2 @@
+package pt.isec.a2017014841.tp2.helpers
+
