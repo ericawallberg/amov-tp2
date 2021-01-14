5764ae309df927ffbebd375a0618196813dba89a
onikenx <onikenx@protonmail.com>
2021-01-14 12:57:04 +0000

    Merge branch 'rework'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/pt/isec/a2017014841/tp2/Game/GameViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/Loading/LoadingViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingActivity.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingClientActivity.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingServerViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/MainActivity.kt
    #	app/src/main/res/layout/activity_loading_server.xml
    #	app/src/main/res/layout/activity_new_game.xml

---
+++ app/src/main/java/pt/isec/a2017014841/tp2/Authentication.kt
@@ -0,0 +1,28 @@
+package pt.isec.a2017014841.tp2
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import pt.isec.a2017014841.tp2.Dados.RC_SIGN_IN
+
+class Authentication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.AnonymousBuilder().build()
+        )
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+}
