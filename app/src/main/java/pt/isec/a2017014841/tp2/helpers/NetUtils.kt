0154f0b5cbfe850d4581c62719644c06857ac2fd
onikenx <onikenx@protonmail.com>
2021-01-14 12:57:04 +0000

    Merge branch 'rework'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/pt/isec/a2017014841/tp2/Game/GameViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/Loading/LoadingViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingActivity.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingClientActivity.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingServerViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/MainActivity.kt
    #	app/src/main/res/layout/activity_loading_server.xml
    #	app/src/main/res/layout/activity_new_game.xml

--- app/src/main/java/pt/isec/a2017014841/tp2/helpers/NetUtils.kt
+++ app/src/main/java/pt/isec/a2017014841/tp2/helpers/NetUtils.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import androidx.lifecycle.MutableLiveData
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.concurrent.thread
@@ -45,13 +48,13 @@ object NetUtils {
     }
 
 
-
     fun getDataAsync(strURL: String, result: MutableLiveData<String?>) {
         thread {
             val strContent = getData(strURL)
             result.postValue(strContent)
         }
     }
+
     fun verifyNetworkState(context: Context): Boolean {
         val connMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
         val networkInfo = connMgr?.activeNetworkInfo
@@ -60,16 +63,29 @@ object NetUtils {
         return false
     }
 
-    fun verifyNetworkStateV2(context: Context):Boolean {
+    fun verifyNetworkStateV2(context: Context): Boolean {
         val connMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         connMgr.allNetworks.forEach { network ->
             connMgr.getNetworkCapabilities(network).apply {
                 if (this?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true ||
-                        this?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true)
+                    this?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true
+                )
                     return true
             }
         }
         return false
     }
 
+    fun getPublicIp(): String {
+        var line : String = ""
+        val url = URL("http://ifconfig.me")
+        try {
+            BufferedReader(InputStreamReader(url.openStream(), "UTF-8")).use { reader ->
+                line = reader.readLine()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return line
+    }
 }
