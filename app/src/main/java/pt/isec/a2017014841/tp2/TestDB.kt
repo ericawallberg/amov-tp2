9370581b684c539f6e87036c8b31a6c60e70a86b
onikenx <onikenx@protonmail.com>
2021-01-14 12:57:04 +0000

    Merge branch 'rework'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/pt/isec/a2017014841/tp2/Game/GameViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/Loading/LoadingViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingActivity.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingClientActivity.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingServerViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/LoadingViewModel.kt
    #	app/src/main/java/pt/isec/a2017014841/tp2/MainActivity.kt
    #	app/src/main/res/layout/activity_loading_server.xml
    #	app/src/main/res/layout/activity_new_game.xml

--- app/src/main/java/pt/isec/a2017014841/tp2/TestDB.kt
+++ app/src/main/java/pt/isec/a2017014841/tp2/TestDB.kt
@@ -1,67 +1,25 @@
 package pt.isec.a2017014841.tp2
 
 import android.os.Bundle
-import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
-import com.google.firebase.firestore.Source
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.ktx.app
-import kotlinx.android.synthetic.main.activity_test_d_b.*
-import pt.isec.a2017014841.tp2.Dados.JogosCollection
 
 class TestDB : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_d_b)
-    //        (1..100)
-//            .forEach { i ->
-//                textview = TextView(this)
-//                textview.setText("texto $i")
-//                activitydb_layout.addView(textview)
-//            }
-    }
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-    override fun onStart() {
-        super.onStart()
-        criarbase_button.setOnClickListener{ onCreateDados() }
-        updatebase_button.setOnClickListener{ onUpdateDados()}
-        deletebase_button.setOnClickListener{onDeleteDados()}
-    }
-
-    fun addview(text : String){
-        activitydb_layout.addView(TextView(this).apply {setText(text)})
-    }
-
-    var dadoN = 1;
-    val db = Firebase.firestore
-    fun onCreateDados(){
-
-        val scores = hashMapOf(
-                "nossa" to 0,
-                "caralho" to 0
-    ***REMOVED***
-        db.collection(JogosCollection).document("e").set(scores)
-    }
-
-    fun onUpdateDados(){
-        val db = Firebase.firestore
-        val
-                v = db.collection("").document("Level1")
-        db.runTransaction {transition ->
-            val doc = transition.get(v)
-            var geopoint = 2
-            transition.update(v,"nrgames",geopoint)
-
-            null
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
-    }
-    fun onDeleteDados(){
-        db.collection("Amov").document("e").delete()
+
+
+
+
+
     }
 }
