b7cbced2b242e20a2a20141287750bdfc32ba2cd
onikenx <onikenx@protonmail.com>
2021-01-16 23:23:04 +0000

    lol estou para levar um tiro

--- app/src/main/java/pt/isec/a2017014841/tp2/MainActivity.kt
+++ app/src/main/java/pt/isec/a2017014841/tp2/MainActivity.kt
@@ -1,9 +1,14 @@
 package pt.isec.a2017014841.tp2
 
+import android.Manifest
 import android.Manifest.permission.*
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -17,33 +22,49 @@ import pt.isec.a2017014841.tp2.Dados.CLIENT_MODE
 import pt.isec.a2017014841.tp2.Dados.PERMISSION_REQUEST_CODE
 import pt.isec.a2017014841.tp2.Dados.RC_SIGN_IN
 import pt.isec.a2017014841.tp2.Dados.SERVER_MODE
+import pt.isec.a2017014841.tp2.Dados.actualLocation
 import pt.isec.a2017014841.tp2.Dados.errorDialog
 import pt.isec.a2017014841.tp2.Loading.LoadingActivity
 import pt.isec.a2017014841.tp2.helpers.NetUtils
 
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
 
     private val TEST_DB = -1;
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(ActivityCompat.checkSelfPermission(this, ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED &&
-                        ActivityCompat.checkSelfPermission(this, INTERNET) != PackageManager.PERMISSION_GRANTED &&
-                        ActivityCompat.checkSelfPermission(this, ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED &&
-                        ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                ACCESS_NETWORK_STATE
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                INTERNET
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                ACCESS_WIFI_STATE
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             errorDialog("Ask Permissions", "Accept permissions for the app to work", this)
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //setup permissions
-        while (ActivityCompat.checkSelfPermission(this, ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, INTERNET) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(READ_SMS, READ_PHONE_NUMBERS, READ_PHONE_STATE, ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_CODE)}
         if (mAuth == null)
             mAuth = FirebaseAuth.getInstance()
         //verifica se tem rede
@@ -71,29 +92,67 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
+
+        //setup permissions
+        while (ActivityCompat.checkSelfPermission(
+                this,
+                ACCESS_NETWORK_STATE
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                INTERNET
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                ACCESS_WIFI_STATE
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    READ_SMS,
+                    READ_PHONE_NUMBERS,
+                    READ_PHONE_STATE,
+                    ACCESS_COARSE_LOCATION,
+                    ACCESS_FINE_LOCATION
+                ),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+
         val currentUser = mAuth!!.currentUser
         if (currentUser == null) {
             val tag = "Authenticação Anonima"
             mAuth?.signInAnonymously()
-                    ?.addOnCompleteListener(this) { task ->
-                        val CollectionGames = "Jogos"
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(tag, "signInAnonymously:success")
-                            val user = mAuth?.currentUser
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(tag, "signInAnonymously:failure", task.exception)
-                            Toast.makeText(
-                                    baseContext, "Authentication failed.",
-                                    Toast.LENGTH_SHORT
-                            ).show()
-                        }
+                ?.addOnCompleteListener(this) { task ->
+                    val CollectionGames = "Jogos"
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(tag, "signInAnonymously:success")
+                        val user = mAuth?.currentUser
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(tag, "signInAnonymously:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
+                }
         }
 
-        if (currentUser != null)
+        if (currentUser != null) {
             Toast.makeText(baseContext, "User logged.", Toast.LENGTH_SHORT).show()
+            activateLocationlistener()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -115,33 +174,35 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+
+    override fun onLocationChanged(location: Location) {
+        actualLocation = location
+    }
+
+    fun activateLocationlistener() {
+        val lm = getSystemService(AppCompatActivity.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    ACCESS_COARSE_LOCATION,
+                    ACCESS_FINE_LOCATION
+                ),
+                PERMISSION_REQUEST_CODE
+            )
+
+
+            return
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 5f, this)
+    }
 }
 
-
-/*
-val btnew_game: Button = findViewById(R.id.btnew_game)
-btnew_game.setOnClickListener{
-    val intent = Intent(this, NewGameActivity::class.java)
-    startActivity(intent)
-}
-
-val btjoin_game: Button = findViewById(R.id.btjoin_game)
-btjoin_game.setOnClickListener{
-    val mDialogView = LayoutInflater.from(this).inflate(R.layout.insert_ip, null);
-    val mBuilder = AlertDialog.Builder(this)
-        .setView(mDialogView)
-        .setTitle("Indique o IP:")
-
-    val mAlertDialog = mBuilder.show()
-    mDialogView.ok.setOnClickListener{
-        mAlertDialog.dismiss()
-        val ip = mDialogView.ip.text.toString()
-        val intent = Intent(this, JoinGameActivity::class.java)
-        intent.putExtra("Ip", ip)
-        startActivity(intent)
-
-    }
-    mDialogView.cancel_button.setOnClickListener{
-        mAlertDialog.dismiss()
-    }
-    */
